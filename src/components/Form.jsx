4ecfca811cb8aef6e1384a16973e309c7974ec10
Hellraiser997 <felipesg03@gmail.com>
2023-10-15 19:13:00 -0300

    Feat: importação de componentes da biblioteca MUI

---
+++ src/components/Form.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
+export default function BasicTextFields() {
+  return (
+    <div>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+    <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
