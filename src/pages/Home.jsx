1d4ae355e76ac80ca24d30c8aad13a094d910359
Hellraiser997 <felipesg03@gmail.com>
2023-10-15 19:39:43 -0300

    Feat: criada const para adição de tarefas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from '../components/Form'
 import ListaItem from '../components/ListaItem'
+import { List } from '@mui/material';
 
 
 export default function Home() {
+    const [tarefas, setTarefas] = useState([]);
+
+    const adicionarTarefa = (tarefa) => {
+        console.log(tarefa)
+        setTarefas([...tarefas, tarefa])
+    }
+
   return (
     <div>
-        <Form />
-        <ListaItem />
+        <Form adicionarTarefa={adicionarTarefa} />
+        <List>
+            {tarefas.map((tarefa) => (
+                <div>
+                    <ListaItem />
+                </div>
+            ))}
+        </List>
     </div>
   )
 }
