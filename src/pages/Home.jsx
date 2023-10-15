0f50a8151947961691734ac08013149447d9a673
Hellraiser997 <felipesg03@gmail.com>
2023-10-15 19:25:24 -0300

    Feat: Criada pasta pages e arquivo Home.jsx para funções

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../components/Form'
+import ListaItem from '../components/ListaItem'
+
+
+export default function Home() {
+  return (
+    <div>
+        <Form />
+        <ListaItem />
+    </div>
+  )
+}
