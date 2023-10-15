3e7506b6bae8b48a6af5a6928826a374d353e47c
Hellraiser997 <felipesg03@gmail.com>
2023-10-15 20:06:27 -0300

    Feat: criada função para adicionar tarefas

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,29 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 
-export default function Form() {
+export default function Form({ adicionarTarefa }) {
+    const [texto, setTexto] = useState(null);
+    const [id, setId] = useState(0)
+    
+    const criarTarefa = (texto) => {
+        const objetoTarefa = { texto: texto, id: id };
+        setId(id + 1);
+        adicionarTarefa(objetoTarefa);
+    }
   return (
+    <Paper>
     <div>
         <TextField 
+        onChange={(e) => setTexto(e.target.value)}
         id="filled-basic" 
         label="Filled" 
         variant="filled" />
-        <Button variant="outlined">Outlined</Button>
+        <Button 
+        variant="outlined" 
+        onClick={() => criarTarefa(texto)}>Outlined</Button>
      </div>    
+    </Paper>
   )
 }
 
