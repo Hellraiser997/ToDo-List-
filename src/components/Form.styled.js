16df0267c20138af850226166ead1c17a5bba90e
Hellraiser997 <felipesg03@gmail.com>
2023-10-26 23:37:37 -0300

    :sparkles: feat: criada pasta e inserida estilização do form

---
+++ src/components/Form.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    width: auto;
+    height: 100%;
+    margin: 0 auto;
+    padding: 0 20px;
+    justify-content: center;      
+`
