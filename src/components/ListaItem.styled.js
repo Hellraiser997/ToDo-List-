4a3f8d61091fbe622af266b39be66c0ae2df6a1f
Hellraiser997 <felipesg03@gmail.com>
2023-10-26 23:50:12 -0300

    :sparkles: feat: criada pasta e adicionada estilização de ListaItem

---
+++ src/components/ListaItem.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    margin-bottom: 5px;
+    justify-content: center;
+
+
+`
